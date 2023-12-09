167c8f5c06263b12523a7ce7f63265918faed70c
chuckcharlie <charles@chuckcharlie.com>
2023-12-09 15:55:44 -0700

    remove avahi for test

--- root/root/run_cups.sh
+++ root/root/run_cups.sh
@@ -34,6 +34,6 @@ if [ `ls -l /config/cupsd.conf 2>/dev/null | wc -l` -ne 0 ]; then
     cp /config/cupsd.conf /etc/cups/cupsd.conf
 fi
 
-/usr/sbin/avahi-daemon --daemonize
+
 /root/printer-update.sh &
 exec /usr/sbin/cupsd -f
